2fb2d6ba9be50445b33fdd56048e3b93b992f1a4
jadentheanalyst <jadenduytran92@gmail.com>
2023-03-17 05:06:06 -0700

    class_work

---
+++ Week 5/InClass/project2.py
@@ -0,0 +1,6 @@
+def message1():
+    lastname = input('enter the lastname: ')
+    firstname = input('enter the firstname: ')
+    print('the lastname is ', lastname, 'the firstname is ' + firstname)
+
+message1()
